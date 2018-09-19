bb82e634872190fcbcec6014db5e480e7c7cd8f8
Marko Babic <wizprogbg@gmail.com>
2018-09-19 13:07:45 +0200

    First commit

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("NewIndex");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(User u)
+        {
+            TestDatabaseEntities1 db = new TestDatabaseEntities1();
+
+            User us = new Models.User();
+            us.Password = u.Password;
+            us.Username = u.Username;
+
+            db.Users.Add(us);
+            db.SaveChanges();
+
+
+
+            return View("Index");
+        }
+    }
+}
